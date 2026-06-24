4ab5a4a3efe4452184b3ef3b33ceb23aa04c4ada
Yash Agrahri <yash5650agrahari@gmail.com>
2026-06-25 01:38:15 +0530

    Security: Completely remove hardcoded credentials

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-
+load_dotenv()
 # Connects directly to your Ecommerce MySQL database
-DATABASE_URL = "mysql+pymysql://root:Yash%407529@localhost/Ecommerce"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
